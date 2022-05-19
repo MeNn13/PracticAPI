d2e7470bf3fbe6bbe01f8f23130c9ca4b7af2134
MeNn13 <banzai.davai@gmail.com>
2022-05-19 18:50:28 +0300

    Добавли контроллеры для: учителя, ученика и родителя

---
+++ Practic/Controllers/StudentAndParentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Practic.Models;
+
+namespace Practic.Controllers
+{
+    [Route("api/ht")]
+    [Authorize(Roles = "Администратор, Ученик, Родитель")]
+    [ApiController]
+    public class StudentAndParentController : ControllerBase
+    {
+        ApplicationContext context;
+        public StudentAndParentController (ApplicationContext _context)
+        {
+            context = _context;
+        }
+    }
+}
---
+++ Practic/Controllers/TeacherController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Practic.Models;
+
+namespace Practic.Controllers
+{
+    [Route("api/teach")]
+    [Authorize(Roles = "Завуч, Администратор, Учитель")]
+    [ApiController]
+    public class TeacherController : ControllerBase
+    {
+        ApplicationContext context;
+        public TeacherController(ApplicationContext _context)
+        {
+            context = _context;
+        }
+    }
+}
